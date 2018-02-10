dfb895640171861ea492bf91a49fea059aec00aa
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-02-10 21:51:03 +0100

    Removed unused Masonry js

--- js/main.js
+++ js/main.js
@@ -4,14 +4,11 @@ $(document).ready(function(){
 
   // initialize Masonry after all images have loaded
   $container.imagesLoaded( function() {
-      var $grid = $container.masonry({
+      $container.masonry({
       // options
       itemSelector: '.grid-item',
       columnWidth: '.grid-sizer',
-      isFitWidth: true,
-      stagger: 30,
-      visibleStyle: { transform: 'translateY(0)', opacity: 1 },
-      hiddenStyle: { transform: 'translateY(100px)', opacity: 0 }
+      isFitWidth: true
     });
   });
 

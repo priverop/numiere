69b72a2194e9a8da33bea83249a417a2867cbd90
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-02-10 14:04:02 +0100

    Empty layout with jQuery, Bootstrap, Masonry & Magnific Popup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+    // with jQuery
+  var $container = $('.grid');
 
+  // initialize Masonry after all images have loaded
+  $container.imagesLoaded( function() {
+      $container.masonry({
+      // options
+      itemSelector: '.grid-item',
+      columnWidth: 300
+    });
+  });
+
+
+});
